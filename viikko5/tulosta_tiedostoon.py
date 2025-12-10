3f4aa96940e8a77b1a77691e6bfbd5f25fc56844
Nea Järvenpää <nea.jarvenpaa@centria.fi>
2025-12-10 14:51:48 +0200

    parannuksia

--- viikko5/tulosta_tiedostoon.py
+++ viikko5/tulosta_tiedostoon.py
@@ -2,9 +2,11 @@
 # This code is licensed under the MIT License.
 
 from datetime import datetime, date
+from pathlib import Path
+import sys
 
 def muunna_tiedot(viikkotiedosto: list) -> list:
-    """ Tämä muuntaa tietorivien tietotyypit oikeiksi"""
+    """ Tämä muuntaa tietorivien tietotyypit oikeiksi """
     muutoslista = []
     muutoslista.append(datetime.fromisoformat(viikkotiedosto[0]))
     muutoslista.append(int(viikkotiedosto[1]))
@@ -14,7 +16,6 @@ def muunna_tiedot(viikkotiedosto: list) -> list:
     muutoslista.append(int(viikkotiedosto[5]))
     muutoslista.append(int(viikkotiedosto[6]))
     return muutoslista
-
 
 def lue_data(viikkotiedosto: str) -> list:
     """Tämä lukee viikkotiedoston ja palauttaa sopivassa rakenteessa"""
@@ -71,41 +72,52 @@ def muodosta_paivalista(lukemat: list) -> list[date]:
     paivat = {lukema[0].date() for lukema in lukemat}
     return sorted(paivat)
 
-def tulosta_paiva_rivi(pv_nimi: str, pvm_str: str, arvot: list[float]) -> None:
+def tulosta_paiva_rivi(pv_nimi: str, pvm_str: str, arvot: list[float], out=sys.stdout) -> None:
     """Tämä tulostaa yhden päivän rivin siististi."""
     muotoilut = [f"{x:.2f}".replace('.', ',') for x in arvot]
     print(
         f"{pv_nimi:<12} {pvm_str:<12}  "
         f"{muotoilut[0]:>7}  {muotoilut[1]:>7}  {muotoilut[2]:>7}   "
-        f"{muotoilut[3]:>7} {muotoilut[4]:>7} {muotoilut[5]:>7}"
+        f"{muotoilut[3]:>7} {muotoilut[4]:>7} {muotoilut[5]:>7}",
+        file=out
     )
 
-def tulosta_otsikko(otsikko: str) -> None:
-    """Tämä tulostaa raportin otsikon ja taulukon otsakerivit."""
-    print(otsikko)
-    print("Päivä        Pvm           Kulutus [kWh]                 Tuotanto [kWh]")
-    print("             (pv.kk.vvvv)    v1       v2        v3          v1     v2     v3")
-    print("---------------------------------------------------------------------------")
+def tulosta_otsikko(otsikko: str, out=sys.stdout) -> None:
+    """Tämä tulostaa raportin tietorivin tiedostoon."""
+    print(otsikko, file=out)
+    print("Päivä        Pvm           Kulutus [kWh]                 Tuotanto [kWh]", file=out)
+    print("             (pv.kk.vvvv)    v1       v2        v3          v1     v2     v3", file=out)
+    print("---------------------------------------------------------------------------", file=out)
+
+def _tyokansio() -> Path:
+   """Tämä palauttaa työskentelykansion"""
+   return Path.cwd()
+
 
 def main():
-    """Tämä tulostaa raportit kolmesta eri tiedostosta."""
+    """Tämä tulostaa raporttiin tiedot viikottain."""
     csv_tiedostot = [
         ("Viikon 41 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", "viikko41.csv"),
         ("Viikon 42 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", "viikko42.csv"),
         ("Viikon 43 sähkönkulutus ja -tuotanto (kWh, vaiheittain)", "viikko43.csv"),
     ]
 
-    for otsikko, polku in csv_tiedostot:
-        lukemat = lue_data(polku)
-        print()  
-        tulosta_otsikko(otsikko)
-        kaikki_paivat = muodosta_paivalista(lukemat)
-        for pvm in kaikki_paivat:
-            pvm_str = f"{pvm.day:02d}.{pvm.month:02d}.{pvm.year}"
-            pv_nimi = viikonpaivat[pvm.weekday()]
-            arvot = paivan_tiedot(pvm_str, lukemat)
-            tulosta_paiva_rivi(pv_nimi, pvm_str, arvot)
-        print("---------------------------------------------------------------------------")
+    raportti_polku = _tyokansio() / "yhteenveto.txt"
+
+    with raportti_polku.open("w", encoding="utf-8", newline="\n") as tiedosto:
+        for otsikko, polku in csv_tiedostot:
+            print("", file=tiedosto)
+            lukemat = lue_data(polku)
+            tulosta_otsikko(otsikko, out=tiedosto)
+            kaikki_paivat = muodosta_paivalista(lukemat)
+            for pvm in kaikki_paivat:
+                pvm_str = f"{pvm.day:02d}.{pvm.month:02d}.{pvm.year}"
+                pv_nimi = viikonpaivat[pvm.weekday()]
+                arvot = paivan_tiedot(pvm_str, lukemat)
+                tulosta_paiva_rivi(pv_nimi, pvm_str, arvot, out=tiedosto)
+            print("---------------------------------------------------------------------------", file=tiedosto)
+
+    print(f"raportti luotu!")
 
 if __name__ == "__main__":
     main()
